ecc86086757c9e5e3e8a0fc0f10cb263c718468b
Just Rufus <130498545+JustsRufus@users.noreply.github.com>
2025-12-05 16:35:43 +0200

    avancing with the inner box section 2 of the dyBox

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 const   dyBoxContainer = select ('.dyBox-container')
+        closingCurtain = select ('.dyBox-container .closing-curtain'),
         dyBox = select ('.dyBox'),
         innerBox_S1 = select('.inner-box.s1'),
+        innerBox_S2 = select('.inner-box.s2'),
         legends = select ('.legends'),
         leftSection = select ('.left-section'),
         uploadBtn = select ('.upload-btn')
@@ -8,14 +10,18 @@ const   dyBoxContainer = select ('.dyBox-container')
 
 uploadBtn.addEventListener('click', () => {
     legends.classList.add('hidden')
-    leftSection.classList.add('hidden')
-    uploadBtn.classList.add('hidden')
+    innerBox_S1.classList.add('hidden')
     dyBoxContainer.classList.add('stretched-full')
+    closingCurtain.classList.remove('hidden')
     setTimeout(() => {
         dyBox.classList.remove('primary')
         dyBox.classList.add('secondary')
     }, 100);
     setTimeout(() => {
         innerBox_S1.classList.add('none')
+        innerBox_S2.classList.remove('none')
     }, 200);
+    setTimeout(() => {
+        innerBox_S2.classList.remove('hidden')
+    }, 300);
 })
