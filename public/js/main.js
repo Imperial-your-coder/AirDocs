94001b2a7ff3c2cb886c1d1095041b7c0b980593
Just Rufus <130498545+JustsRufus@users.noreply.github.com>
2025-12-05 13:41:35 +0200

    updating the dyBox with adding the inside box second section

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,21 @@
-const   dyBox = select ('.dyBox'),
+const   dyBoxContainer = select ('.dyBox-container')
+        dyBox = select ('.dyBox'),
+        innerBox_S1 = select('.inner-box.s1'),
         legends = select ('.legends'),
         leftSection = select ('.left-section'),
         uploadBtn = select ('.upload-btn')
+        
 
 uploadBtn.addEventListener('click', () => {
     legends.classList.add('hidden')
     leftSection.classList.add('hidden')
     uploadBtn.classList.add('hidden')
+    dyBoxContainer.classList.add('stretched-full')
     setTimeout(() => {
         dyBox.classList.remove('primary')
         dyBox.classList.add('secondary')
     }, 100);
+    setTimeout(() => {
+        innerBox_S1.classList.add('none')
+    }, 200);
 })
